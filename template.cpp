ca54739a7ea9298df8c2d03c23918a85544266c6
Smit Rami <smitrami6661@gmail.com>
2021-02-16 01:37:25 +0530

    template add

---
+++ template.cpp
@@ -0,0 +1,75 @@
+
+#define SVR "CreatedBySmitRami"
+
+///////////////////////Loading Payloads.......////////////////////////////
+#include<bits/stdc++.h>
+#define f(i,a,b) for(int i = a; i < b; i++)
+#define fd(i,a,b) for(int i = a; i > b;i--)
+#define ll long long
+#define fl(i,a,b) for(ll i = a; i < b; i++)
+#define VI vector<int>
+#define VP vector<pair<int,int>>
+#define VS vector<string>
+#define VL vector<long long>
+#define VC vector<char>
+#define VF vector<float>
+#define PB push_back
+#define F first
+#define S second
+#define fa(i,skill) for(auto i : skill)
+#define fr(i,skill) for(VI :: reverse_iterator i = skill.rbegin(); i != skill.rend(); i++)
+#define PI pair<int,int>
+#define mi map<int,int>
+#define mci map<char,int>
+#define all(x) x.begin(),x.end()
+#define MAX 1000000007
+using namespace std;
+
+///////////////////////Engine Check.......////////////////////////////
+void edit(VI &skill,int n)
+{
+    f(i,0,n)
+        {
+            int x;
+            cin >> x;
+            skill.PB(x);
+        }
+}
+void editl(VL &skill,ll n)
+{
+    fl(i,0,n)
+        {
+            ll x;
+            cin >> x;
+            skill.PB(x);
+        }
+}
+
+
+
+
+
+void achilles()
+{
+
+}
+
+
+        
+
+///////////////////////3....2....1...Ignition////////////////////////////
+
+int main()
+{
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int t;cin >>t ;
+    while(t--)
+    {achilles();}
+    return 0;
+}
+
+
+
